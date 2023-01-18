06987a2a653f7f644c50e8843580e66a80edf9b5
yuriy-kuzin-webdev <yuriy.kuzin.webdev@gmail.com>
2023-01-18 23:04:18 +0200

    added images, styles

--- client/src/components/Toolbar.jsx
+++ client/src/components/Toolbar.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import '../styles/toolbar.scss'
+import React from "react";
+import "../styles/toolbar.scss";
 
 const Toolbar = () => {
   return (
-    <div className='toolbar'>Toolbar</div>
-  )
-}
+    <div className="toolbar">
+      <button className="toolbar__btn brush" />
+      <button className="toolbar__btn rect" />
+      <button className="toolbar__btn circle" />
+      <button className="toolbar__btn eraser" />
+      <button className="toolbar__btn line" />
+      <input type="color" className="color"/>
+      <button className="toolbar__btn undo" />
+      <button className="toolbar__btn redo" />
+      <button className="toolbar__btn save" />
+    </div>
+  );
+};
 
-export default Toolbar
+export default Toolbar;
